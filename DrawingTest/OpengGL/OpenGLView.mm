3797f6c00ece4f370a14b368830f2a4bbd6174a0
Mostafizur Rahman <mostafizur.cse@gmail.com>
2017-12-17 01:49:55 +0600

    Offscreen texture rendering success.

--- DrawingTest/OpengGL/OpenGLView.mm
+++ DrawingTest/OpengGL/OpenGLView.mm
@@ -10,7 +10,7 @@ typedef struct {
 
 @interface OpenGLView(){
     GLShaderProgram *shaderProgram;
-     GLuint offscreenTexture;
+    GLuint offscreenTexture;
     GLShaderProgram *quadProgram;
     
     CGSize offscreenTextureSize;
@@ -39,7 +39,7 @@ const GLubyte Indices[] = {
     if (self) {
         [self setupLayer];
         [self setupContext];
-        offscreenTextureSize = CGSizeMake(1080, 1822);
+        offscreenTextureSize = CGSizeMake(1080, 1822); // same as img.png resolution, other small images will be scaled up linearly
     }
     return self;
 }
@@ -48,34 +48,25 @@ const GLubyte Indices[] = {
     _eaglLayer = (CAEAGLLayer*) self.layer;
     _eaglLayer.opaque = NO;
     _eaglLayer.drawableProperties =
-    [NSDictionary dictionaryWithObjectsAndKeys: kEAGLColorFormatRGBA8,
-     kEAGLDrawablePropertyColorFormat, nil];
+    [NSDictionary dictionaryWithObjectsAndKeys: kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat, nil];
 }
 
 - (void)setupContext {
-    _context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
-    
-    GLKView* view = (GLKView*) self;
-    view.context = _context;
-    
+    EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
+    _context = [[EAGLContext alloc] initWithAPI:api];
+    self.context = _context;
     if (!_context) {
         NSLog(@"Failed to initialize OpenGLES 2.0 context");
         exit(1);
     }
-    
     if (![EAGLContext setCurrentContext:_context]) {
         NSLog(@"Failed to set current OpenGL context");
         exit(1);
     }
-    
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    self.opaque = NO;
 }
 
 -(void)initializeRenderer{
-//    [self setupRenderBuffer];
     [self setupFrameBuffer];
     shaderProgram = [[GLShaderProgram alloc] initWithVS:@"QuadVProgram" FS:@"QuadFProgram"];
     quadProgram =  [[GLShaderProgram alloc] initWithVS:@"QuadVShader" FS:@"QuadFShader"];
@@ -95,12 +86,6 @@ const GLubyte Indices[] = {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 }
-
-//- (void)setupRenderBuffer {
-//    glGenRenderbuffers(1, &renderBuffer);
-//    glBindRenderbuffer(GL_RENDERBUFFER, renderBuffer);
-//    [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
-//}
 
 - (void)setupFrameBuffer {
     
@@ -122,30 +107,31 @@ const GLubyte Indices[] = {
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, offscreenTexture, 0);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuffer);
     
-
+    
 }
 
-- (GLuint)setupTexture:(NSString *)fileName {
+- (GLuint)setupTexture:(NSString *)imageName {
     // 1
-    CGImageRef spriteImage = [UIImage imageNamed:fileName].CGImage;
-    if (!spriteImage) {
-        NSLog(@"Failed to load image %@", fileName);
+    CGImageRef sourceImage = [UIImage imageNamed:imageName].CGImage;
+    if (!sourceImage) {
+        NSLog(@"Failed to load image %@", imageName);
         exit(1);
     }
     
     // 2
-    size_t width = CGImageGetWidth(spriteImage);
-    size_t height = CGImageGetHeight(spriteImage);
+    size_t width = CGImageGetWidth(sourceImage);
+    size_t height = CGImageGetHeight(sourceImage);
     
     GLubyte * spriteData = (GLubyte *) calloc(width*height*4, sizeof(GLubyte));
     
-    CGContextRef spriteContext = CGBitmapContextCreate(spriteData, width, height, 8, width*4,
-                                                       CGImageGetColorSpace(spriteImage), kCGImageAlphaPremultipliedLast);
+    CGContextRef imageContext = CGBitmapContextCreate(spriteData, width, height, 8, width * 4,
+                                                      CGImageGetColorSpace(sourceImage),
+                                                      kCGImageAlphaPremultipliedLast);
     
     // 3
-    CGContextDrawImage(spriteContext, CGRectMake(0, 0, width, height), spriteImage);
+    CGContextDrawImage(imageContext, CGRectMake(0, 0, width, height), sourceImage);
     
-    CGContextRelease(spriteContext);
+    CGContextRelease(imageContext);
     
     // 4
     GLuint texName;
@@ -157,7 +143,8 @@ const GLubyte Indices[] = {
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (int)width, (int)height, 0, GL_RGBA, GL_UNSIGNED_BYTE, spriteData);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (int)width, (int)height, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, spriteData);
     
     free(spriteData);
     return texName;
@@ -166,9 +153,6 @@ const GLubyte Indices[] = {
 
 //here is offscreen texture drawing.
 -(void)renderTexture{
-    glUseProgram(shaderProgram.shaderHandle);
-    
-    
     
     [self bindDrawable];
     // 1. SAVE OUT THE DEFAULT FRAME BUFFER
@@ -177,14 +161,12 @@ const GLubyte Indices[] = {
     
     // 2. RENDER TO OFFSCREEN RENDER TARGET
     glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-    
     glViewport(0, 0, offscreenTextureSize.width, offscreenTextureSize.height);
-    glClearColor(0, 0, 0, 0);
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
     /// DRAW THE SCENE ///
-    
-    
-    
+    glUseProgram(shaderProgram.shaderHandle);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glEnableVertexAttribArray(shaderProgram.a_TexturePosition);
     glVertexAttribPointer(shaderProgram.a_TexturePosition, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, Position));
@@ -205,74 +187,29 @@ const GLubyte Indices[] = {
     
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
     glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
-//
-//    [self getImage:offscreenTextureSize];
+    
+    //    [self getImage:offscreenTextureSize]; //check GPU image for higher resultion. it should be 1080X1822
     
     
     // 3. RESTORE DEFAULT FRAME BUFFER
     glBindFramebuffer(GL_FRAMEBUFFER, default_frame_buffer);
-    
     // 4. RENDER FULLSCREEN QUAD
     glViewport(0, 0, self.bounds.size.width * 2, self.bounds.size.height * 2);
-    glClearColor(0, 0, 0, 0);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    
-    glBindProgramPipelineEXT(quadProgram.shaderHandle);
-    
+    glUseProgram(quadProgram.shaderHandle);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glEnableVertexAttribArray(quadProgram.a_TexturePosition);
     glVertexAttribPointer(quadProgram.a_TexturePosition, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, Position));
     glEnableVertexAttribArray(quadProgram.a_TextureCoordinate);
     glVertexAttribPointer(quadProgram.a_TextureCoordinate, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, TexCoord));
-    
-    
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, offscreenTexture);
     glUniform1i(quadProgram.u_BackgroundTextureRGB, 0);
-    
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
     glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
-    
-    [self getImage:CGSizeMake( self.bounds.size.width * 2,  self.bounds.size.height * 2)];
-    
-//
     [_context presentRenderbuffer:GL_RENDERBUFFER];
-    
-    
-    
-    
-    
-    
-    /*glViewport(0, 0, self.bounds.size.width * 2, self.bounds.size.height * 2);
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_COLOR);
-    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glEnableVertexAttribArray(shaderProgram.a_TexturePosition);
-    glVertexAttribPointer(shaderProgram.a_TexturePosition, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, Position));
-    glEnableVertexAttribArray(shaderProgram.a_TextureCoordinate);
-    glVertexAttribPointer(shaderProgram.a_TextureCoordinate, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*) offsetof(Vertex, TexCoord));
-    
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glUniform1i(shaderProgram.u_BackgroundTextureRGB, 0);
-    
-    glActiveTexture(GL_TEXTURE0 + 1);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    glUniform1i(shaderProgram.u_FrameTextureRGB, 1);
-    
-    glActiveTexture(GL_TEXTURE0 + 2);
-    glBindTexture(GL_TEXTURE_2D, texture3);
-    glUniform1i(shaderProgram.u_BaseTextureRGB, 2);
-    
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
-    
-    [self getImage:CGSizeMake(1080, 1822)];
-    */
-    
+    //    [self getImage:CGSizeMake( self.bounds.size.width * 2,  self.bounds.size.height * 2)]; // check gpu image for lower resolution
 }
 
 -(UIImage *)getImage:(CGSize)imageSize{
